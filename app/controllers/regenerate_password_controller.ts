e080e48187458f2a2a56e566427903c3da2fd122
princeshiiit <princetigleyinfotech@gmail.com>
2026-01-19 16:31:47 +0800

    Removes unused hash import from regenerate_password_controller

--- app/controllers/regenerate_password_controller.ts
+++ app/controllers/regenerate_password_controller.ts
@@ -1,6 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import User from '#models/user'
-import hash from '@adonisjs/core/services/hash'
+
 
 export default class RegeneratePasswordController {
   /**
